9a861b70aae8df572e57b7ade26591d3fe115fab
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-05 17:13:13 -0500

    feat: root goes to main index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "about", to: "about#index"
+
+  root to: "main#index"
+
   resources :users do
     resources :comments
   end
