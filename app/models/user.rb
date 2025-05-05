2c625e024072fe8b30d17ad0aa6734dac284a532
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-05 16:59:34 -0500

    feat: model validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
+
+  validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 254 }
+  validates :password, presence: true, length: { minimum: 6, maximum: 20 }
 end
