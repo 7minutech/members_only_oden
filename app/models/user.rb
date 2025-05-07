79e08187f9ed0b0e9e242586e6961409d69be89d
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-07 10:09:38 -0500

    feat: commented out name validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
 
-  validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
+  # validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 254 }
   validates :password, presence: true, length: { minimum: 6, maximum: 20 }
 end
