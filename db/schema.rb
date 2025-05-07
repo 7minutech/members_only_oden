337fe88e8bbb2d31fc6dc3248117ac49a5d35a6b
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-07 10:08:25 -0500

    feat: updated schema with no name col for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_07_142947) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_07_145307) do
   create_table "posts", force: :cascade do |t|
     t.text "body", null: false
     t.integer "user_id", null: false
@@ -21,7 +21,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_07_142947) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
